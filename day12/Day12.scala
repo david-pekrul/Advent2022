6bfc783a4ee8940713ba63b861661b6a8b794a41
David Pekrul <dbighead77@gmail.com>
2022-12-18 22:58:19 -0600

    Still working on Day 12. Got the test case working, but real is failing.

--- day12/Day12.scala
+++ day12/Day12.scala
@@ -2,9 +2,11 @@ package day12
 
 import helpers.Helpers
 
+import scala.annotation.tailrec
+
 object Day12 {
   def main(args: Array[String]): Unit = {
-    val rawInputLines = Helpers.readFile("day12/test.txt")
+    val rawInputLines = Helpers.readFile("day12/day12.txt")
     val letterHeights = rawInputLines.zipWithIndex.map { case (rawLine, rowIndex) =>
       rawLine.toCharArray.zipWithIndex.map { case (char, colIndex) => {
         LetterHeight(char, rowIndex, colIndex)
@@ -17,8 +19,40 @@ object Day12 {
     val E = getEndLocation(letterHeightMap)
     MapStats.setMax(letterHeightMap)
 
+    //    println(letterHeightMap)
 
-    println(letterHeightMap)
+    val calculated = bfs(S, letterHeightMap)
+
+    //    println(calculated)
+
+    printMap(calculated)
+
+    val part1 = calculated.get(E.getCoords()).get.depth
+    println(s"Part 1: $part1")
+    //338 too low
+    //470 too high
+
+    val minForLetters = alphabet.map(char => {
+      calculated.values
+        .filter(_.visited())
+        .filter(spot => spot.char == char)
+        .reduce((a,b) => if(a.depth < b.depth) {a} else {b})
+        .printString
+    }).toSeq
+    println(minForLetters)
+
+
+  }
+
+  val alphabet = ('a' to 'z')
+
+  def printMap(input: Map[(Int, Int), LetterHeight]): Unit = {
+    (0 to MapStats.maxRow).foreach(row => {
+      (0 to MapStats.maxCol).foreach(col => {
+        print(input.get(row,col).get.printString + "\t")
+      })
+      println
+    })
   }
 
   def getStartLocation(lm: Map[(Int, Int), LetterHeight]) = {
@@ -29,18 +63,37 @@ object Day12 {
     lm.values.find(lh => lh.char == 'E').get
   }
 
-  def flood(currentLocation: LetterHeight, map: Map[(Int, Int), LetterHeight]): Map[(Int, Int), LetterHeight] = {
-    val pathSoFar = currentLocation.path
-    val neighbors = currentLocation.getNeighborCoords().map(c => map.get(c).get)
-    ???
+  def bfs(start: LetterHeight, map: Map[(Int, Int), LetterHeight]) = {
+
+    @tailrec
+    def bfs2(currentMap: Map[(Int, Int), LetterHeight], nextLayer: Set[(Int, Int)], depth: Int = 0): Map[(Int, Int), LetterHeight] = {
+      val updatedMap = nextLayer.foldLeft(currentMap)((updatedMap, coord) => {
+        updatedMap.updated(coord, updatedMap.get(coord).get.visit(depth))
+      })
+      val x = nextLayer
+        .map(n => {
+          val here = updatedMap.get(n).get
+          val allNeighbors = here.neighborCoords
+          val validSteps = allNeighbors
+            .filter(x => {
+              val neighborHeight = updatedMap.get(x).get.height
+              (neighborHeight <= (here.height + 1))
+            })
+          validSteps
+        })
+        .flatten
+        .map(coord => updatedMap.get(coord).get)
+        .filter(!_.visited())
+      val y = x
+        .map(_.getCoords())
+      if (y.isEmpty) {
+        return updatedMap
+      }
+      bfs2(updatedMap, y, depth + 1)
+    }
+
+    bfs2(map, Set(start.getCoords()))
   }
-
-  //  def getPaths(start: LetterHeight, end: LetterHeight, map: Map[(Int,Int),LetterHeight]): Seq[Seq[LetterHeight]] = {
-  //
-  //    def depthFirstSearch(current: LetterHeight, end: LetterHeight, visited: Seq[LetterHeight])
-  //
-  //  }
-
 }
 
 object MapStats {
@@ -56,33 +109,39 @@ object MapStats {
   }
 }
 
-case class LetterHeight(char: Char, row: Int, col: Int, path: Seq[LetterHeight] = Seq(), visited: Boolean = false) {
-  lazy val height = Helpers.getLetterIndex(char)
+case class LetterHeight(char: Char, row: Int, col: Int, depth: Int = -1) {
+  lazy val height = {
+    char match {
+      case 'E' => 27
+      case 'S' => 0
+      case _ => Helpers.getLetterIndex(char)
+    }
+  }
 
   def getCoords() = (row, col)
 
-  def getNeighborCoords(): Seq[(Int, Int)] = {
-    (row - 1 to row + 1).map(nextRow => {
-      (col - 1 to col + 1).map(nextCol => {
-        if (nextRow < 0 || nextRow > MapStats.maxRow || nextCol < 0 || nextCol > MapStats.maxCol) {
-          None
-        } else if (nextRow == row && nextCol == col) {
-          None
-        } else {
-          Some((nextRow, nextCol))
-        }
-      })
-    }).flatten.filter(_.isDefined).map(_.get)
+  lazy val neighborCoords: Seq[(Int, Int)] = {
+
+    Seq(
+      (row - 1, col),
+      (row + 1, col),
+      (row, col - 1),
+      (row, col + 1)
+    )
+      .filter { case (nextRow, nextCol) => {
+        !(nextRow < 0 || nextRow > MapStats.maxRow || nextCol < 0 || nextCol > MapStats.maxCol)
+      }
+      }
   }
 
-  def addPath(incoming: Seq[LetterHeight]): LetterHeight = {
-    LetterHeight(
-      char,
-      row,
-      col,
-      path = incoming,
-      visited = true
-    )
+  def visited() = depth != -1
+
+  def visit(d: Int): LetterHeight = {
+    LetterHeight(char, row, col, depth = d)
+  }
+
+  def printString = {
+    s"[$char,$depth]"
   }
 }
 
