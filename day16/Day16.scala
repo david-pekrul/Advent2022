c4407165547cdfce8f020271d31672895bc42f25
David Pekrul <dbighead77@gmail.com>
2022-12-27 11:05:27 -0600

    Day 16 part 1 solved

--- day16/Day16.scala
+++ day16/Day16.scala
@@ -7,16 +7,33 @@ object Day16 {
     def rawLines = helpers.Helpers.readFile("day16/day16.txt")
 
     val (valves, network) = ValveNetwork.parse(rawLines)
+    val startingValve = valves.filter(_.id.equals("AA")).head
+    val (flowValves, slimNetwork) = ValveNetwork.slimNetwork(valves, network, startingValve)
+//    println(flowValves.mkString("\r\n"))
+//    println(slimNetwork.toSeq.mkString("\r\n"))
 
-    val (flowValves, slimNetwork) = ValveNetwork.slimNetwork(valves, network)
-    println(flowValves.mkString("\r\n"))
-    println(slimNetwork.toSeq.mkString("\r\n"))
 
+    val paths = ValveNetwork.findPaths(30, startingValve, valves, slimNetwork)
+    val optimalPath = paths
+      .map(p => (ValveNetwork.calculateScore(p), p))
+      .reduce((a, b) => {
+        if (a._1 > b._1) {
+          a
+        } else {
+          b
+        }
+      })
 
+    println(s"Part 1 optimal path: ${optimalPath._2}")
+    println(s"Part 1: ${optimalPath._1}")
   }
 }
 
-case class Valve(id: String, flowRate: Int, openedMinute: Int = 0)
+case class Valve(id: String, flowRate: Int, openedMinute: Int = 0) {
+  def setOpenTime(minute: Int): Valve = {
+    Valve(id, flowRate, minute)
+  }
+}
 
 object ValveNetwork {
   def parse(rawLines: Seq[String]) = {
@@ -42,10 +59,10 @@ object ValveNetwork {
     (valve, Map(valve.id -> linksTo))
   }
 
-  def slimNetwork(valves: Set[Valve], map: Map[String, Seq[String]]): (Set[Valve], Map[String, Map[String, Int]]) = {
+  def slimNetwork(valves: Set[Valve], map: Map[String, Seq[String]], startValve: Valve): (Set[Valve], Map[String, Map[String, Int]]) = {
     //slim the network down so we just have the non-zero flow rate valves, and the weighted min distances between just them
 
-    val flowValves = valves.filter(_.flowRate > 0)
+    val flowValves = valves.filter(_.flowRate > 0) + startValve
 
     val slimedValves = flowValves.map(flowValve => {
       bfs(flowValve, valves, map)
@@ -58,7 +75,7 @@ object ValveNetwork {
     (flowValves, mergedSlimValves)
   }
 
-  def bfs(start: Valve, allValves: Set[Valve], originalMap: Map[String, Seq[String]]): Map[String, Map[String, Int]] = {
+  private def bfs(start: Valve, allValves: Set[Valve], originalMap: Map[String, Seq[String]]): Map[String, Map[String, Int]] = {
 
     val allValveDict = allValves.map(x => (x.id -> x)).toMap
 
@@ -89,9 +106,58 @@ object ValveNetwork {
     val allDistances = _bfs(Seq(start.id), 0, Map())
     //remove zero-flow valves
 
-    allDistances.updated(start.id,allDistances.get(start.id).get.filter(target => {
+    allDistances.updated(start.id, allDistances.get(start.id).get.filter(target => {
       allValveDict.get(target._1).get.flowRate > 0 && !target._1.equals(start.id)
     }))
 
   }
+
+  def findPaths(minutes: Int, startValve: Valve, allValves: Set[Valve], slimNetwork: Map[String, Map[String, Int]]): Seq[Seq[Valve]] = {
+
+    val allValveDict = allValves.map(x => (x.id -> x)).toMap
+
+    def _step(pathSoFar: Seq[Valve], nextValve: Valve, minutesRemaining: Int): Seq[Seq[Valve]] = {
+
+      if (minutesRemaining <= 0) {
+        return Seq(pathSoFar)
+      }
+
+      //don't waste time opening the starting 0-flow-rate valve
+      val timeAfterOpeningValve = minutesRemaining - (if (nextValve.flowRate == 0) {
+        0
+      } else {
+        1
+      })
+      val updatedPath = pathSoFar :+ nextValve.setOpenTime(timeAfterOpeningValve)
+
+      val allNeighborsAndCosts = slimNetwork.get(nextValve.id).get.toSeq.map(n => (allValveDict.get(n._1).get, n._2))
+
+      val visitedNeighborIds = pathSoFar.map(_.id).toSet
+      val unvisitedNeighbors = allNeighborsAndCosts.filter(n => {
+        !visitedNeighborIds.contains(n._1.id)
+      })
+
+      if (unvisitedNeighbors.isEmpty) {
+        //we have run out of valves to open.
+        //We don't know if the valves were opened in the most optimal order though!
+        return Seq(updatedPath)
+      }
+
+      val furtherPaths = unvisitedNeighbors.map(neighborAndCost => {
+        val (neighborValve, neighborCost) = neighborAndCost
+        _step(updatedPath, neighborValve, timeAfterOpeningValve - neighborCost)
+      }).flatten
+
+      furtherPaths
+    }
+
+    _step(Seq(), startValve, minutes)
+  }
+
+  def calculateScore(path: Seq[Valve]): Int = {
+    path.map(v => {
+      v.flowRate * v.openedMinute
+    }).sum
+  }
+
 }
