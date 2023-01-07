04e58f11f5e83cf6fe02232fbda5039c74635d4b
David Pekrul <david.pekrul@gmail.com>
2023-01-07 14:39:07 -0600

    Day 21 part 1 solved

--- day21/Day21.scala
+++ day21/Day21.scala
@@ -4,7 +4,8 @@ import helpers.Helpers
 
 object Day21 {
   def main(args: Array[String]): Unit = {
-    val rawLines = Helpers.readFile("day21/test.txt")
+    //    val rawLines = Helpers.readFile("day21/test.txt")
+    val rawLines = Helpers.readFile("day21/day21.txt")
 
     val monkeyMap = rawLines
       .map(parse)
@@ -12,18 +13,32 @@ object Day21 {
         acc + (monkey.id -> monkey)
       })
 
-    println(monkeyMap.mkString("\r\n"))
+    //    println(monkeyMap.mkString("\r\n"))
 
     val rootMonkey = monkeyMap("root")
 
-
-
+    val part1 = calc(rootMonkey, monkeyMap)
+    println(s"Part 1: ${part1._1}")
   }
 
-  def putMonkeysIntoTree(monkeyMap: Map[String, Monkey]) = {
+  def calc(root: Monkey, monkeyMap: Map[String, Monkey]) = {
 
-    def _dfs()
 
+    def _dfs(currentMonkey: Monkey, updatedMap: Map[String, Monkey]): (Long, Map[String, Monkey]) = {
+      if (currentMonkey.num.isDefined) {
+        (currentMonkey.num.get, updatedMap)
+      } else {
+        //get left value
+        val (leftVal, leftUpdatedMap) = _dfs(updatedMap(currentMonkey.leftId.get), updatedMap)
+        val (rightVal, rightUpdatedMap) = _dfs(leftUpdatedMap(currentMonkey.rightId.get), leftUpdatedMap)
+        val (computedVal, updatedCurrent) = currentMonkey.performOp(leftVal, rightVal)
+
+        val updatedMapAfterCurrent = rightUpdatedMap.updated(updatedCurrent.id, updatedCurrent)
+        (computedVal, updatedMapAfterCurrent)
+      }
+    }
+
+    _dfs(root, monkeyMap)
   }
 
   def parse(rawLine: String) = {
@@ -37,7 +52,7 @@ object Day21 {
       Monkey(id, leftId = Some(left), rightId = Some(right), op = Some(op))
     } else {
       val numberRegex(id, num) = rawLine
-      Monkey(id, num = Some(num))
+      Monkey(id, num = Some(num * 1L))
     }
   }
 }
@@ -46,6 +61,20 @@ case class Monkey(id: String,
                   leftId: Option[String] = None,
                   rightId: Option[String] = None,
                   op: Option[String] = None,
-                  num: Option[Int] = None) {
+                  num: Option[Long] = None) {
+
+  def performOp(leftVal: Long, rightVal: Long): (Long, Monkey) = {
+    val computedNum = op match {
+      case None => throw new RuntimeException("No operation defined")
+      case Some("*") => leftVal * rightVal
+      case Some("+") => leftVal + rightVal
+      case Some("-") => leftVal - rightVal
+      case Some("/") => leftVal / rightVal
+      case Some(x) => throw new RuntimeException(s"Unrecognized $x")
+    }
+
+    (computedNum, Monkey(id = id, leftId = leftId, rightId = rightId, op = op, num = Some(computedNum)))
+  }
+
 
 }
